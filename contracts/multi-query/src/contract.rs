a74334e5406e0b386dcddb0644fd264e19110df8
TrevorJTClarke <trevorjtclarke@gmail.com>
2022-05-20 12:37:27 -0700

    working through msg injector

--- contracts/multi-query/src/contract.rs
+++ contracts/multi-query/src/contract.rs
@@ -1,15 +1,31 @@
 use oorandom::Rand32;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cosmwasm_std::{
+    from_binary, to_binary, Binary, CosmosMsg, Deps, DepsMut, Env, MessageInfo, Response,
+    StdResult, SubMsg, WasmQuery,
+};
 use cw2::set_contract_version;
+use cw_storage_plus::Item;
 
 use crate::error::ContractError;
 use crate::msg::{
-    ExecuteMsg, InstantiateMsg, QueryMsg, QueryMultiTestResponse, QueryTestResponse,
-    RandomResponse, RuleResponse,
+    ExecuteMsg, InstantiateMsg, QueryMsg, QueryTestResponse, RandomResponse, RuleResponse,
 };
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct State {
+    pub msg_cosmos: CosmosMsg,
+    pub msg_sub: SubMsg,
+    pub msg_binary: Binary,
+    pub msg_query: WasmQuery,
+}
+
+pub const STATE: Item<State> = Item::new("state");
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:multi-query";
@@ -45,9 +61,10 @@ pub fn execute(
 pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
         QueryMsg::GetRandom {} => to_binary(&query_random(env)?),
-        QueryMsg::GetBoolBinary {} => to_binary(&query_bool_binary()?),
-        QueryMsg::QueryTest {} => to_binary(&query_test(deps, env)?),
-        QueryMsg::QueryMultiTest {} => to_binary(&query_multi_test(deps, env)?),
+        QueryMsg::GetBoolBinary { msg } => to_binary(&query_bool_binary_echo(msg)?),
+        QueryMsg::GetInputBoolBinary { msg } => to_binary(&query_input_bool_binary(msg)?),
+        QueryMsg::QueryChain {} => to_binary(&query_chain(deps, env)?),
+        QueryMsg::QueryConstruct {} => to_binary(&query_construct(deps, env)?),
     }
 }
 
@@ -62,20 +79,94 @@ fn query_random(env: Env) -> StdResult<RandomResponse> {
     })
 }
 
-fn query_bool_binary() -> StdResult<RuleResponse<Option<Binary>>> {
-    Ok((true, None))
+fn query_bool_binary_echo(msg: Option<Binary>) -> StdResult<RuleResponse<Option<Binary>>> {
+    Ok((true, msg))
 }
 
-fn query_test(deps: Deps, env: Env) -> StdResult<QueryTestResponse> {
-    let msg = QueryMsg::GetRandom {};
-    let res: RuleResponse<Option<Binary>> =
-        deps.querier.query_wasm_smart(&env.contract.address, &msg)?;
-    let data = format!("{:?}", res.0);
+fn query_input_bool_binary(msg: Binary) -> StdResult<RuleResponse<Option<Binary>>> {
+    let something: RandomResponse = from_binary(&msg)?;
+    let data = Some(to_binary(&something)?);
+    Ok((true, data))
+}
+
+// GOAL:
+// Parse a generic query response, and inject input for the next query
+fn query_chain(deps: Deps, env: Env) -> StdResult<QueryTestResponse> {
+    // Get known format for first msg
+    let msg1 = QueryMsg::GetRandom {};
+    let res1: RandomResponse = deps
+        .querier
+        .query_wasm_smart(&env.contract.address, &msg1)?;
+
+    // Query a bool with some data from previous
+    let msg2 = QueryMsg::GetBoolBinary {
+        msg: Some(to_binary(&res1)?),
+    };
+    let res2: RuleResponse<Option<Binary>> = deps
+        .querier
+        .query_wasm_smart(&env.contract.address, &msg2)?;
+
+    // Utilize previous query for the input of this query
+    // TODO: Setup binary msg, parse into something that contains { msg }, then assign the new binary response to it (if any)
+    // let msg = QueryMsg::GetInputBoolBinary {
+    //     msg: Some(to_binary(&res2)?),
+    // };
+    // let res: RuleResponse<Option<Binary>> =
+    //     deps.querier.query_wasm_smart(&env.contract.address, &msg)?;
+
+    // Format something to read results
+    let data = format!("{:?}", res2);
     Ok(QueryTestResponse { data })
 }
 
-fn query_multi_test(_deps: Deps, _env: Env) -> StdResult<QueryMultiTestResponse> {
-    Ok(QueryMultiTestResponse { data: vec![] })
+// create a smart query into binary
+fn query_construct(_deps: Deps, _env: Env) -> StdResult<QueryTestResponse> {
+    let input_binary = to_binary(&RandomResponse { number: 1235 })?;
+
+    // create an injectable blank msg
+    let json_msg = json!({
+        "get_random": {
+            "msg": "",
+            "key": "value"
+        }
+    });
+    // blank msg to binary
+    let msg_binary = to_binary(&json_msg.to_string())?;
+
+    // try to parse binary
+    let msg_unbinary: String = from_binary(&msg_binary)?;
+    // let msg_parsed: Value = serde_json::from_str(msg_unbinary);
+    let msg_parse = serde_json::from_str(msg_unbinary.as_str());
+    let mut msg_parsed: String = "".to_string();
+
+    // Attempt to peel the onion, and fill with goodness
+    if let Ok(msg_parse) = msg_parse {
+        let parsed: Value = msg_parse;
+        // travel n1 child keys
+        if parsed.is_object() {
+            for (_key, value) in parsed.as_object().unwrap().iter() {
+                for (k, _v) in value.as_object().unwrap().iter() {
+                    // check if this key has "msg"
+                    if k == "msg" {
+                        // then replace "msg" with "input_binary"
+                        // TODO:
+                        // parsed[key][k] = input_binary;
+                        msg_parsed = input_binary.to_string();
+                    }
+                }
+            }
+        }
+    }
+
+    // Lastly, attempt to make the actual query!
+    // let res1 = deps
+    //     .querier
+    //     .query_wasm_smart(&env.contract.address, &msg1)?;
+
+    // Format something to read results
+    // let data = format!("{:?}", res1);
+    let data = format!("{:?} :: {:?}", msg_binary, msg_parsed);
+    Ok(QueryTestResponse { data })
 }
 
 // #[cfg(test)]
--- contracts/multi-query/src/msg.rs
+++ contracts/multi-query/src/msg.rs
@@ -1,3 +1,4 @@
+use cosmwasm_std::Binary;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -14,9 +15,10 @@ pub enum ExecuteMsg {
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
     GetRandom {},
-    GetBoolBinary {},
-    QueryTest {},
-    QueryMultiTest {},
+    GetBoolBinary { msg: Option<Binary> },
+    GetInputBoolBinary { msg: Binary },
+    QueryChain {},
+    QueryConstruct {},
 }
 
 // We define a custom struct for each query response
