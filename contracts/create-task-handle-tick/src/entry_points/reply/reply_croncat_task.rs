0c73d57ad29f9423f4e4fd7007391b5a0b18b1c8
Mike Purvis <mikedotexe@gmail.com>
2023-04-21 10:36:09 -0700

    handle tick: reply_handle_task_creation » reply_handle_croncat_task_creation

--- contracts/create-task-handle-tick/src/entry_points/reply/reply_croncat_task.rs
+++ contracts/create-task-handle-tick/src/entry_points/reply/reply_croncat_task.rs
@@ -1,12 +1,12 @@
 use crate::errors::ContractError;
 use cosmwasm_std::{DepsMut, Reply, Response};
 // CRONCAT HELPER
-use croncat_integration_utils::reply_handler::reply_handle_task_creation;
+use croncat_integration_utils::reply_handler::reply_handle_croncat_task_creation;
 
 pub fn reply(_deps: DepsMut, msg: Reply) -> Result<Response, ContractError> {
     // Pass the reply message into a CronCat integration helper
     // This returns helpful information about the task including hash, owner, etc.
-    let (_task_info, msg_binary) = reply_handle_task_creation(msg)?;
+    let (_task_info, msg_binary) = reply_handle_croncat_task_creation(msg)?;
 
     // YOUR CODE HERE
 
